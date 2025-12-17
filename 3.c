c2a6568acece23864486310073affa6a31b4bb1b
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-17 05:52:49 +0530

    Add initial implementation of data types demonstration

---
+++ 3.c
@@ -0,0 +1,15 @@
+// Data Types
+
+#include <stdio.h>
+int main() {
+    int number = 10;
+    float decimal = 5.5;
+    char character = 'A';
+    double largeDecimal = 20.99;
+
+    printf("Integer: %d\n", number);
+    printf("Float: %.2f\n", decimal);
+    printf("Character: %c\n", character);
+    printf("Double: %.2lf\n", largeDecimal);
+    return 0;
+}
