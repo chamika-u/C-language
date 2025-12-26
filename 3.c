fbd0f2c44f22ce240c575d51f397ae9931d17f15
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-27 00:42:35 +0530

    Reorganize output statements for improved clarity in data types demonstration

--- 3.c
+++ 3.c
@@ -11,25 +11,25 @@ int main() {
 
     printf("Integer: %d\n", number); // integer
 
-    printf("Float: %.0f\n", decimal); // no decimal places
+    printf("\nFloat: %.0f\n", decimal); // no decimal places
     printf("Float: %.1f\n", decimal); // .1 for one decimal place
     printf("Float: %.2f\n", decimal); //.2 for two decimal places
     printf("Float: %.3f\n", decimal); //.3 for three decimal places
 
-    printf("Syntax Float: %f\n", syntaxFloat); // default precision
+    printf("\nSyntax Float: %f\n", syntaxFloat); // default precision
     printf("Syntax Double: %.3f\n", syntaxDouble); // .3 for three decimal places
 
-    printf("Character: %c\n", character); //Character 
+    printf("\nCharacter: %c\n", character); //Character 
     printf("Character ASCII: %d\n", character); //Character ASCII value
 
-    printf("Double: %.0lf\n", largeDecimal); // no decimal places
+    printf("\nDouble: %.0lf\n", largeDecimal); // no decimal places
     printf("Double: %.1lf\n", largeDecimal); //.1 for one decimal place 
     printf("Double: %.2lf\n", largeDecimal); //.2 for two decimal places
     printf("Double: %.3lf\n", largeDecimal); //.3 for three decimal places
     printf("Double: %.5lf\n", largeDecimal); //.5 for five decimal places
     printf("Double: %lf\n", largeDecimal); // default precision
 
-    printf("Size of int: %zu bytes\n", sizeof(number)); // size of int
+    printf("\nSize of int: %zu bytes\n", sizeof(number)); // size of int
     printf("Size of float: %zu bytes\n", sizeof(decimal)); // size of float
     printf("Size of char: %zu bytes\n", sizeof(character)); // size of char
     printf("Size of double: %zu bytes\n", sizeof(largeDecimal)); // size of double 
