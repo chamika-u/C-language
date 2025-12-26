ec4e81a5ac4e04c91a2937bc263530ca69228357
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-27 01:32:20 +0530

    Add initial implementation of C program with comments and variable declarations

---
+++ 5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// This is a comment line in C code
+// You can use comments to explain your code
+// You can use comments for documentation purposes
+// Comments are ignored by the compiler
+// Single-line comments start with //
+// Multi-line comments are enclosed between /* and */
+
+/*  This is a 
+   multi-line comment
+   in C programming language
+*/
+
+// You can use comments to remove code temporarily
+
+int main() {
+    // Variable declaration
+    int a = 5;          // Integer variable
+    float b = 3.14f;   // Float variable
+    double c = 2.71828; // Double variable
+    char d = 'Z';      // Character variable
+
+    // Printing values of variables
+    printf("Integer a: %d\n", a);
+    
+   // In here to run this following code you must remove the comment marks
+
+   /* printf("Float b: %.2f\n", b); // .2 for two decimal places
+    printf("Double c: %.5lf\n", c); // .5 for five decimal places
+    printf("Character d: %c\n", d); */ 
+
+    return 0;
+}
