378982ff393e49ab515811a7cee8dd8b57926e6a
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-27 01:15:11 +0530

    Add user input and output functionality for age, marks, average, and grade

---
+++ 4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    //variable declarion
+    int age;
+    float marks;
+    double average;
+    char grade;
+
+    //getting inputs from user for each variables
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("\nEnter your makrs: ");
+    scanf("%f", &marks);
+
+    printf("\nEnter your average: ");
+    scanf("%lf", &average);
+
+    printf("\nEnter your garde: ");
+    scanf(" %c", &grade);
+
+    //printing all the user input 
+    printf("Your age is %d\n", age);
+    printf("Your marks is %f\n", marks);
+    printf("Your average us %lf\n", average);
+    printf("Your grade is %c", grade);
+
+}
