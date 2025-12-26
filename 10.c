2b79b915470785be06241269e90a0916abb06cb3
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-27 05:07:24 +0530

    Refactor voting eligibility check into a new file and implement user input handling

--- 10.c
+++ 10.c
@@ -1,24 +1,23 @@
-// Ternerrary operator and boolean operations in C
+// To check whether number is a positive, negative or zero.
 
 #include <stdio.h>
 #include <stdbool.h>
 
 int main() {
+
     // variable declaration
-    int age;
-    int votingAge = 18;
-    int answer;
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
 
-    // user inputs
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    printf("\nAre you a citizen? (1 for yes, 0 for no): ");
-    scanf("%d", &answer);
-
-    // using ternary operator to check voting eligibility
-    (age >= votingAge && answer == 1) ? 
-        printf("You are eligible to vote.\n") : 
-        printf("You are not eligible to vote.\n");
-
-    return 0;
+    // check if the number is positive, negative or zero
+    if (number > 0) {
+        printf("The number %d is positive.\n", number);
+    } 
+    else if (number < 0) {
+        printf("The number %d is negative.\n", number);
+    } 
+    else {
+        printf("The number is zero.\n");
+    }
 }
