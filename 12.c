765edcf66d06ffbd013d4d970210feb4c5c5319c
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-27 05:13:57 +0530

    Add implementation to check if a number is even or odd

---
+++ 12.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    // variable declaration
+    int number1;
+
+    // user inputs
+    printf("Enter the integer: ");
+    scanf("%d", &number1);
+
+    // check if number even or odd
+    (number1 % 2 == 0) ? 
+        printf("The number %d is even.\n", number1) : 
+        printf("The number %d is odd.\n", number1);
+    
+    return 0;
+}
