4f45e077a1e31c2b3f6e73e4aa833073504f56e1
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-27 01:53:24 +0530

    Add initial file for type conversion in C

---
+++ 7.c
@@ -0,0 +1,20 @@
+// Type conversion in c | explicit and implicit
+
+/*Data type hierarchy
+    long double
+    double
+    float
+    unsigned long long int
+    long long int
+    unsigned long int
+    long int
+    unsigned int
+    int
+    unsigned short int
+    short int
+    char
+*/
+
+#include <stdio.h>
+
+
