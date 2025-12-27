bc9f595beac88cbea2ee9889a4a405f8b945eca1
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-28 03:38:56 +0530

    Add implementation of for loop to print numbers from 1 to 5

---
+++ 19-for-loops.c
@@ -0,0 +1,14 @@
+// for loops in C
+
+#include <stdio.h>
+
+int main() {
+
+    // variable declaration
+    int i;
+
+    // for loop to print numbers from 1 to 5
+    for (i = 1; i <= 5; i++) {
+        printf("%d\n", i);
+    }
+}
