f184497e6c7ac69069e1d7231db22feda59b3d3b
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-28 01:34:54 +0530

    Add while loop implementation to print numbers from 1 to 5

---
+++ 16-while-loop.c
@@ -0,0 +1,17 @@
+// while loop in C
+
+#include <stdio.h>
+
+int main() {
+
+    // variable declaration
+    int count = 1;
+
+    // while loop to print numbers from 1 to 5
+    while (count <= 5) {
+        printf("%d\n", count);
+        count++;
+    }
+
+    return 0;
+}
