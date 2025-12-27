5e91dde6cb234538f865c0a60f45afa6462e3d23
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-28 00:01:03 +0530

    Add switch statement implementation to display the day of the week based on user input

---
+++ 13.c
@@ -0,0 +1,39 @@
+// switch statement in C
+
+#include <stdio.h>
+
+int main() {
+
+    // variable declaration
+    int day;
+    printf("Enter a number 1 to 7: ");
+    scanf("%d", &day);
+
+    // switch statement to print day of the week
+    switch (day)
+    {
+    case 1: 
+        printf("Monday\n");
+        break;
+    case 2: 
+        printf("Tuesday\n");
+        break;
+    case 3:
+        printf("Wednesday\n");
+        break;
+    case 4:
+        printf("Thursday\n");
+        break;
+    case 5:
+        printf("Friday\n");
+        break;
+    case 6:
+        printf("Saturday\n");   
+        break;
+    case 7:
+        printf("Sunday\n");
+    default:
+        printf("Invalid input! Please enter a number between 1 and 7.\n");
+        break;
+    }
+}
