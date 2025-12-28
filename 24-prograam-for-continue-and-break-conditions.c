4c43213bcff149fbee8b594bb72aaba264b92df2
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-28 17:58:40 +0530

    Add implementation of a program to print negative odd numbers from user input

---
+++ 24-prograam-for-continue-and-break-conditions.c
@@ -0,0 +1,27 @@
+// program for printing number if it is negative odd from user
+
+#include <stdio.h>
+
+int main() {
+
+    // chechink whether its negative or positive
+    while (1){
+        // variable declaration
+        int number;
+
+        // user input
+        printf("Enter your number: ");
+        scanf("%d", &number);
+
+        // break condition to exit the loop
+        if (number>0) {
+            continue;
+        }
+        
+        // checking whether its odd or even
+        if ((number % 2) != 0) {
+            printf("%d is negative odd number\n", number);
+            break;
+        }  
+    }
+}
