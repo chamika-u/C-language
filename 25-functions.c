38b68228ef32d7c97c06e877af0284c21a890183
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-28 19:28:36 +0530

    Fix comment formatting in main function for clarity

--- 25-functions.c
+++ 25-functions.c
@@ -12,6 +12,5 @@ int main () {
     greet(); // calling function "grret"
     greet(); // we can call a function as much as we want inside of the main without type manually what function does. 
     greet(); // we create function only onetime. This saves time and devs can deliver clean code. 
-    printf("This works in main. Not inside of a function.");
-    
+    printf("This works in main. Not inside of a function."); // since this is not a function calling, this works directly in main. No need to call.  
 }
