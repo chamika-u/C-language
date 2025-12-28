aa691c4a052be837c5a9bfcf2101640a2c46473b
Chamika Udayanga <c.udayanga2k04@gmail.com>
2025-12-28 19:24:10 +0530

    Add implementation of a simple function to greet and demonstrate function calls in C

---
+++ 25-functions.c
@@ -0,0 +1,17 @@
+// simple function in c
+
+#include <stdio.h>
+
+// create a function called greet
+void greet() {
+    printf("Hello World! I am a function! \n");
+}
+
+// main 
+int main () {
+    greet(); // calling function "grret"
+    greet(); // we can call a function as much as we want inside of the main without type manually what function does. 
+    greet(); // we create function only onetime. This saves time and devs can deliver clean code. 
+    printf("This works in main. Not inside of a function.");
+    
+}
