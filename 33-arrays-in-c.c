c6c65df455df1e83561c34f1373dae5a6a67b0d9
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-15 04:24:28 +0530

    Add implementation of array usage in C

---
+++ 33-arrays-in-c.c
@@ -0,0 +1,10 @@
+// arrays in C
+#include <stdio.h>
+int main() {
+    int age[5]; // declaring an array of integers to hold ages
+    int marks[5] = {22, 23, 24, 25}; // declaring and assigning values to an array
+    printf("marks of student 1: %d\n", marks[0]); // accessing first element of the array
+    printf("marks of student 2: %d\n", marks[1]); // accessing second element of the array
+
+    return 0;
+}
