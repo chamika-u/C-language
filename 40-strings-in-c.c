84ab279e717018641e62966e8f758ac857b931ac
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-15 15:34:55 +0530

    Add implementation to demonstrate basic string operations in C

---
+++ 40-strings-in-c.c
@@ -0,0 +1,10 @@
+// this demonstrates basic string operations in C using the C17 standard
+
+#include <stdio.h>
+#include <string.h> // Including string header file for string operations
+
+int main() {
+    
+    char str[] = "I Love You!"; // declaring and initializing a string
+    printf("%s", str); // printing the string
+}
