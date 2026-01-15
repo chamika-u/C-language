a023c29fabae183cac03b78c4b107652ceb348dd
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-16 02:15:50 +0530

    Fix formatting of address output in pointer demonstration program

--- 48-pointers-inc.c
+++ 48-pointers-inc.c
@@ -4,10 +4,11 @@
 
 int main() {
     int age = 25;
-    printf("%p", &age); // print the address of age variable
+    printf("Adress: %p \n", &age); // print the address of age variable
 
     int* ptr = &age; // pointer variable ptr holds the address of age variable
-    printf("\n%p", ptr); // print the address stored in pointer variable ptr
-     
+    printf("Adress: %p \n", ptr); // print the address stored in pointer variable ptr
+    
+    printf("Value: %d \n", *ptr); // print the value at the address stored in pointer variable ptr
     return 0;
 }
