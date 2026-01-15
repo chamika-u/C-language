f1f1f2ba50684a14e87712160aa3c6d0c40856ea
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-16 02:16:40 +0530

    Add demonstration of modifying pointer value in C program

--- 48-pointers-inc.c
+++ 48-pointers-inc.c
@@ -10,5 +10,9 @@ int main() {
     printf("Adress: %p \n", ptr); // print the address stored in pointer variable ptr
     
     printf("Value: %d \n", *ptr); // print the value at the address stored in pointer variable ptr
+    
+    *ptr = 30; // changing the value at the address stored in pointer variable ptr
+    printf("New Value: %d \n", age); // print the new value of age variable
+
     return 0;
 }
