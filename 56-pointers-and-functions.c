f02fc2cb19028c92e28acd50ea8fca7a0f833a86
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-22 05:22:10 +0530

    Add program to demonstrate changing a value using pointers and functions

---
+++ 56-pointers-and-functions.c
@@ -0,0 +1,24 @@
+// this program demonstartes change value of a number using pointers and functions
+
+#include <stdio.h>
+
+void findValue (int* num){
+    *num= 55;
+}
+int main() {
+
+    // variable declarion
+    int number = 12;
+
+    // print number
+    printf("Number: %d \n", number); 
+
+    // calling the function
+    findValue(&number);
+
+    // print changed value
+    printf("Chnaged Number: %d", number);
+
+    return 0;
+
+}
