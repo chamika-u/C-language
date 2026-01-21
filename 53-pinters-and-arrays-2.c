b870cea04f7eff0c879de3bab01e8d5250db3efa
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-22 04:02:38 +0530

    Fix comments and streamline pointer address printing in array

--- 53-pinters-and-arrays-2.c
+++ 53-pinters-and-arrays-2.c
@@ -7,17 +7,10 @@ int main () {
     // array declaration
     int array [5] = {1,2,3,4,5};
 
-    // print pointer adress for each item in array
+    // print array adress for each item in array
     for (int i=0; i<5; i++){
-        printf("%d = %p\n", array[i], &array[i]);
+        printf("Array Address for %d = %p\n", array[i], array + i);
     }
-
-    printf("Array Adress of 1: %p \n", array);
-    printf("Array Adress of 2: %p \n", array+1);
-    printf("Array Adress of 3: %p \n", array+2);
-    printf("Array Adress of 4: %p \n", array+3);
-    printf("Array Adress of 5: %p \n", array+4);
-
 
     return 0;
 
