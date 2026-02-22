14b33c9c61f0facbcf6fff3adb4e5e46c934a4ed
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-02-23 04:00:10 +0530

    Add program to find square of a number using pointers and functions

---
+++ 08-pointers/find-square-pointers-and-functions.c
@@ -0,0 +1,17 @@
+// this program demostrates how to find square of a number using pointers and functions
+
+#include <stdio.h>
+
+int* findSquare(int* num) {
+    int square = (*num) * (*num); // calculate the square of the number
+    *num = square; // update the value at the pointer to the square
+
+    return num; // return the pointer to the squared value
+}
+
+int main() {
+    int number = 12; // initialize the number to be squared
+    int* result = findSquare(&number); // pass the address of the number to the function and get the pointer to the squared value
+    printf("The square of the number is: %d\n", number); // print the squared value using the original variable, which has been updated by the function
+    return 0;
+}
